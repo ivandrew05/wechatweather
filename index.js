281660adba402b9ee749e77e903c6a72501a45fd
ivandrew05 <46248495+ivandrew05@users.noreply.github.com>
2020-05-31 15:24:17 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var bmap = require('../../libs/bmap-wx.js');
 Page({
 
   //分享功能
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     let that = this;
     return {
       title: '分享',
@@ -19,11 +19,15 @@ Page({
             })
             console.log(that.setData.isShow)
           },
-          fail: function (res) { console.log(res) },
-          complete: function (res) { console.log(res) }
+          fail: function(res) {
+            console.log(res)
+          },
+          complete: function(res) {
+            console.log(res)
+          }
         })
       },
-      fail: function (res) {
+      fail: function(res) {
         console.log(res)
       }
     }
@@ -50,12 +54,24 @@ Page({
       console.log(data)
     };
 
-    var success = function(data) {
+    // 获取地图数据
+    var mapSuccess = function(data) {
+      console.log(data)
+      var mapData = data.originalData.result.addressComponent;
+      var location = mapData.city + ' ' + mapData.district;
+
+      that.setData({
+        location: location,
+      })
+    }
+
+    // 获取天气数据
+    var weatherSuccess = function(data) {
       console.log(data)
       var weatherData = data.originalData.results[0];
-      
+
       that.setData({
-        location: weatherData.currentCity,
+        // location: weatherData.currentCity,
         currentTemperature: that.reverseTemperature(weatherData.weather_data[0].temperature),
         description: weatherData.weather_data[0].weather,
         wind: weatherData.weather_data[0].wind,
@@ -91,13 +107,20 @@ Page({
     // 发起weather请求 
     BMap.weather({
       fail: fail,
-      success: success
+      success: weatherSuccess
     });
+
+    // 发起regeocoding检索请求 
+    BMap.regeocoding({
+      fail: fail,
+      success: mapSuccess,
+    });
+
   },
 
   // 计算滚动区域的高度
   calculateScrollHeight() {
-    let windowHeight = wx.getSystemInfoSync().windowHeight 
+    let windowHeight = wx.getSystemInfoSync().windowHeight
     let windowWidth = wx.getSystemInfoSync().windowWidth
     // console.log(windowHeight);
     // console.log(windowWidth);
@@ -109,46 +132,41 @@ Page({
       scrollHeight: scrollHeight,
     });
   },
-  
-  reverseTemperature(temperature){
+
+  reverseTemperature(temperature) {
     var low;
     var high;
     var result;
     var index = temperature.indexOf("~");
     var length = temperature.length;
 
-    low = temperature.substring(index+2, length-1);
-    high = temperature.substring(0, index-1);
+    low = temperature.substring(index + 2, length - 1);
+    high = temperature.substring(0, index - 1);
     result = low + " - " + high + "℃";
     return result;
   },
 
-  getWeatherDate(date){
+  getWeatherDate(date) {
     var index = date.indexOf("(")
     var todayDate
-    todayDate = date.substring(0, index-1);
+    todayDate = date.substring(0, index - 1);
     return todayDate;
   },
 
-  getAirClass(pm25){
+  getAirClass(pm25) {
     var airClass = "";
 
     if (pm25 <= 50) {
       airClass = "优";
-    }
-    else if (pm25 > 50 && pm25 <= 100) {
+    } else if (pm25 > 50 && pm25 <= 100) {
       airClass = "良";
-    }
-    else if (pm25 > 100 && pm25 <= 150) {
+    } else if (pm25 > 100 && pm25 <= 150) {
       airClass = "轻度污染";
-    }
-    else if (pm25 > 150 && pm25 <= 200) {
+    } else if (pm25 > 150 && pm25 <= 200) {
       airClass = "中度污染";
-    }
-    else if (pm25 > 200 && pm25 <= 300) {
+    } else if (pm25 > 200 && pm25 <= 300) {
       airClass = "重度污染";
-    }
-    else {
+    } else {
       airClass = "严重污染";
     }
     // airClass="优"
@@ -160,34 +178,29 @@ Page({
 
     if (pm25 <= 50) {
       airColor = "#009966";
-    }
-    else if (pm25 > 50 && pm25 <= 100) {
+    } else if (pm25 > 50 && pm25 <= 100) {
       airColor = "#ffde33";
-    }
-    else if (pm25 > 100 && pm25 <= 150) {
+    } else if (pm25 > 100 && pm25 <= 150) {
       airColor = "#ff9933";
-    }
-    else if (pm25 > 150 && pm25 <= 200) {
+    } else if (pm25 > 150 && pm25 <= 200) {
       airColor = "#cc0033";
-    }
-    else if (pm25 > 200 && pm25 <= 300) {
+    } else if (pm25 > 200 && pm25 <= 300) {
       airColor = "#660099";
-    }
-    else {
+    } else {
       airColor = "#7e0023";
     }
     // airColor ="#7e0023"
     return airColor;
   },
 
-  pmTap(){
+  pmTap() {
     console.log("成功")
     wx.showModal({
       title: '代码中',
       content: '敬请期待',
     })
   },
-  
+
   getWeatherIconDay: function(description) {
     var condition = String(description);
     var conditionDay = String(description);
@@ -199,83 +212,58 @@ Page({
 
       if (conditionDay == "晴") {
         url = "/images/01-sunny.svg";
-      } 
-      else if (conditionDay == "多云") {
+      } else if (conditionDay == "多云") {
         url = "/images/02-duoyun.svg";
-      } 
-      else if (conditionDay == "阴") {
+      } else if (conditionDay == "阴") {
         url = "/images/03-yin.svg";
-      } 
-      else if (conditionDay == "阵雨") {
+      } else if (conditionDay == "阵雨") {
         url = "/images/04-zhenyu.svg";
-      }
-      else if (conditionDay == "雷阵雨") {
+      } else if (conditionDay == "雷阵雨") {
         url = "/images/05-leizhenyu.svg";
-      }
-      else if (conditionDay == "雷阵雨伴有冰雹") {
+      } else if (conditionDay == "雷阵雨伴有冰雹") {
         url = "/images/06-leizhenyubingbao";
-      }
-      else if (conditionDay == "雨夹雪") {
+      } else if (conditionDay == "雨夹雪") {
         url = "/images/07-yujiaxue.svg";
-      }
-      else if (conditionDay == "小雨") {
+      } else if (conditionDay == "小雨") {
         url = "/images/08-xiaoyu.svg";
-      } 
-      else if (conditionDay == "中雨") {
+      } else if (conditionDay == "中雨") {
         url = "/images/09-zhongyu.svg";
-      } 
-      else if (conditionDay == "大雨") {
+      } else if (conditionDay == "大雨") {
         url = "/images/10-dayu.svg";
-      } 
-      else if (conditionDay == "暴雨") {
+      } else if (conditionDay == "暴雨") {
         url = "/images/11-baoyu.svg";
-      }
-      else if (conditionDay == "大暴雨") {
+      } else if (conditionDay == "大暴雨") {
         url = "/images/12-dabaoyu.svg";
-      }
-      else if (conditionDay == "特大暴雨") {
+      } else if (conditionDay == "特大暴雨") {
         url = "/images/13-tedabaoyu.svg";
-      }
-      else if (conditionDay == "阵雪") {
+      } else if (conditionDay == "阵雪") {
         url = "/images/14-zhenxue.svg";
-      }
-      else if (conditionDay == "小雪") {
+      } else if (conditionDay == "小雪") {
         url = "/images/15-xiaoxue.svg";
-      }
-      else if (conditionDay == "中雪") {
+      } else if (conditionDay == "中雪") {
         url = "/images/16-zhongxue.svg";
-      }
-      else if (conditionDay == "大雪") {
+      } else if (conditionDay == "大雪") {
         url = "/images/17-daxue.svg";
-      }
-      else if (conditionDay == "暴雪") {
+      } else if (conditionDay == "暴雪") {
         url = "/images/18-baoxue.svg";
-      }
-      else if (conditionDay == "雾") {
+      } else if (conditionDay == "雾") {
         url = "/images/19-wu.svg";
-      }
-      else if (conditionDay == "冻雨") {
+      } else if (conditionDay == "冻雨") {
         url = "/images/20-dongyu.svg";
-      }
-      else if (conditionDay == "沙尘暴") {
+      } else if (conditionDay == "沙尘暴") {
         url = "/images/21-shachenbao.svg";
-      }
-      else if (conditionDay == "浮尘") {
+      } else if (conditionDay == "浮尘") {
         url = "/images/30-fuchen.svg";
-      }
-      else if (conditionDay == "扬沙") {
+      } else if (conditionDay == "扬沙") {
         url = "/images/31-yangsha.svg";
-      }
-      else if (conditionDay == "强沙尘暴") {
+      } else if (conditionDay == "强沙尘暴") {
         url = "/images/32-qiangshachenbao.svg";
-      }
-      else if (conditionDay == "霾") {
+      } else if (conditionDay == "霾") {
         url = "/images/33-mai.svg";
-      }
-      else{
+      } else {
         url = "/images/unknow.svg";
       }
-      
+
     }
 
     // not include 转
@@ -284,80 +272,55 @@ Page({
 
       if (condition == "晴") {
         url = "/images/01-sunny.svg";
-      }
-      else if (condition == "多云") {
+      } else if (condition == "多云") {
         url = "/images/02-duoyun.svg";
-      }
-      else if (condition == "阴") {
+      } else if (condition == "阴") {
         url = "/images/03-yin.svg";
-      }
-      else if (condition == "阵雨") {
+      } else if (condition == "阵雨") {
         url = "/images/04-zhenyu.svg";
-      }
-      else if (condition == "雷阵雨") {
+      } else if (condition == "雷阵雨") {
         url = "/images/05-leizhenyu.svg";
-      }
-      else if (condition == "雷阵雨伴有冰雹") {
+      } else if (condition == "雷阵雨伴有冰雹") {
         url = "/images/06-leizhenyubingbao";
-      }
-      else if (condition == "雨夹雪") {
+      } else if (condition == "雨夹雪") {
         url = "/images/07-yujiaxue.svg";
-      }
-      else if (condition == "小雨") {
+      } else if (condition == "小雨") {
         url = "/images/08-xiaoyu.svg";
-      }
-      else if (condition == "中雨") {
+      } else if (condition == "中雨") {
         url = "/images/09-zhongyu.svg";
-      }
-      else if (condition == "大雨") {
+      } else if (condition == "大雨") {
         url = "/images/10-dayu.svg";
-      }
-      else if (condition == "暴雨") {
+      } else if (condition == "暴雨") {
         url = "/images/11-baoyu.svg";
-      }
-      else if (condition == "大暴雨") {
+      } else if (condition == "大暴雨") {
         url = "/images/12-dabaoyu.svg";
-      }
-      else if (condition == "特大暴雨") {
+      } else if (condition == "特大暴雨") {
         url = "/images/13-tedabaoyu.svg";
-      }
-      else if (condition == "阵雪") {
+      } else if (condition == "阵雪") {
         url = "/images/14-zhenxue.svg";
-      }
-      else if (condition == "小雪") {
+      } else if (condition == "小雪") {
         url = "/images/15-xiaoxue.svg";
-      }
-      else if (condition == "中雪") {
+      } else if (condition == "中雪") {
         url = "/images/16-zhongxue.svg";
-      }
-      else if (condition == "大雪") {
+      } else if (condition == "大雪") {
         url = "/images/17-daxue.svg";
-      }
-      else if (condition == "暴雪") {
+      } else if (condition == "暴雪") {
         url = "/images/18-baoxue.svg";
-      }
-      else if (condition == "雾") {
+      } else if (condition == "雾") {
         url = "/images/19-wu.svg";
-      }
-      else if (condition == "冻雨") {
+      } else if (condition == "冻雨") {
         url = "/images/20-dongyu.svg";
-      }
-      else if (condition == "沙尘暴") {
+      } else if (condition == "沙尘暴") {
         url = "/images/21-shachenbao.svg";
-      }
-      else if (condition == "浮尘") {
+      } else if (condition == "浮尘") {
         url = "/images/30-fuchen.svg";
-      }
-      else if (condition == "扬沙") {
+      } else if (condition == "扬沙") {
         url = "/images/31-yangsha.svg";
-      }
-      else if (condition == "强沙尘暴") {
+      } else if (condition == "强沙尘暴") {
         url = "/images/32-qiangshachenbao.svg";
-      }
-      else if (condition == "霾") {
+      } else if (condition == "霾") {
         url = "/images/33-mai.svg";
-      }
-      else {
+      } else {
         url = "/images/unknow.svg";
       }
     }
@@ -367,172 +330,122 @@ Page({
   getWeatherIconNight: function(description) {
     var condition = String(description);
     var conditionNight = String(description);
-    var url= "";
+    var url = "";
 
     if (condition.includes("转")) {
       conditionNight = condition.substring(condition.indexOf("转") + 1, condition.length);
       // console.log(conditionNight);
 
       if (conditionNight == "晴") {
-        url= "/images/34-clearnight.svg";
-      }
-      else if (conditionNight == "多云") {
+        url = "/images/34-clearnight.svg";
+      } else if (conditionNight == "多云") {
         url = "/images/35-duoyunnight.svg";
-      }
-      else if (conditionNight == "阴") {
+      } else if (conditionNight == "阴") {
         url = "/images/36-yinnight.svg";
-      }
-      else if (conditionNight == "阵雨") {
+      } else if (conditionNight == "阵雨") {
         url = "/images/37-zhenyunight.svg";
-      }
-      else if (conditionNight == "雷阵雨") {
+      } else if (conditionNight == "雷阵雨") {
         url = "/images/05-leizhenyu.svg";
-      }
-      else if (conditionNight == "雷阵雨伴有冰雹") {
+      } else if (conditionNight == "雷阵雨伴有冰雹") {
         url = "/images/06-leizhenyubingbao";
-      }
-      else if (conditionNight == "雨夹雪") {
+      } else if (conditionNight == "雨夹雪") {
         url = "/images/07-yujiaxue.svg";
-      }
-      else if (conditionNight == "小雨") {
+      } else if (conditionNight == "小雨") {
         url = "/images/08-xiaoyu.svg";
-      }
-      else if (conditionNight == "中雨") {
+      } else if (conditionNight == "中雨") {
         url = "/images/09-zhongyu.svg";
-      }
-      else if (conditionNight == "大雨") {
+      } else if (conditionNight == "大雨") {
         url = "/images/10-dayu.svg";
-      }
-      else if (conditionNight == "暴雨") {
+      } else if (conditionNight == "暴雨") {
         url = "/images/11-baoyu.svg";
-      }
-      else if (conditionNight == "大暴雨") {
+      } else if (conditionNight == "大暴雨") {
         url = "/images/12-dabaoyu.svg";
-      }
-      else if (conditionNight == "特大暴雨") {
+      } else if (conditionNight == "特大暴雨") {
         url = "/images/13-tedabaoyu.svg";
-      }
-      else if (conditionNight == "阵雪") {
+      } else if (conditionNight == "阵雪") {
         url = "/images/38-zhenxuenight.svg";
-      }
-      else if (conditionNight == "小雪") {
+      } else if (conditionNight == "小雪") {
         url = "/images/15-xiaoxue.svg";
-      }
-      else if (conditionNight == "中雪") {
+      } else if (conditionNight == "中雪") {
         url = "/images/16-zhongxue.svg";
-      }
-      else if (conditionNight == "大雪") {
+      } else if (conditionNight == "大雪") {
         url = "/images/17-daxue.svg";
-      }
-      else if (conditionNight == "暴雪") {
+      } else if (conditionNight == "暴雪") {
         url = "/images/18-baoxue.svg";
-      }
-      else if (conditionNight == "雾") {
+      } else if (conditionNight == "雾") {
         url = "/images/19-wu.svg";
-      }
-      else if (conditionNight == "冻雨") {
+      } else if (conditionNight == "冻雨") {
         url = "/images/20-dongyu.svg";
-      }
-      else if (conditionNight == "沙尘暴") {
+      } else if (conditionNight == "沙尘暴") {
         url = "/images/21-shachenbao.svg";
-      }
-      else if (conditionNight == "浮尘") {
+      } else if (conditionNight == "浮尘") {
         url = "/images/30-fuchen.svg";
-      }
-      else if (conditionNight == "扬沙") {
+      } else if (conditionNight == "扬沙") {
         url = "/images/31-yangsha.svg";
-      }
-      else if (conditionNight == "强沙尘暴") {
+      } else if (conditionNight == "强沙尘暴") {
         url = "/images/32-qiangshachenbao.svg";
-      }
-      else if (conditionNight == "霾") {
+      } else if (conditionNight == "霾") {
         url = "/images/33-mai.svg";
-      }
-      else {
+      } else {
         url = "/images/unknow.svg";
       }
-    } 
-    
+    }
+
     // not include 转
     else {
       // console.log(condition);
 
       if (condition == "晴") {
         url = "/images/34-clearnight.svg";
-      }
-      else if (condition == "多云") {
+      } else if (condition == "多云") {
         url = "/images/35-duoyunnight.svg";
-      }
-      else if (condition == "阴") {
+      } else if (condition == "阴") {
         url = "/images/36-yinnight.svg";
-      }
-      else if (condition == "阵雨") {
+      } else if (condition == "阵雨") {
         url = "/images/37-zhenyunight.svg";
-      }
-      else if (condition == "雷阵雨") {
+      } else if (condition == "雷阵雨") {
         url = "/images/05-leizhenyu.svg";
-      }
-      else if (condition == "雷阵雨伴有冰雹") {
+      } else if (condition == "雷阵雨伴有冰雹") {
         url = "/images/06-leizhenyubingbao";
-      }
-      else if (condition == "雨夹雪") {
+      } else if (condition == "雨夹雪") {
         url = "/images/07-yujiaxue.svg";
-      }
-      else if (condition == "小雨") {
+      } else if (condition == "小雨") {
         url = "/images/08-xiaoyu.svg";
-      }
-      else if (condition == "中雨") {
+      } else if (condition == "中雨") {
         url = "/images/09-zhongyu.svg";
-      }
-      else if (condition == "大雨") {
+      } else if (condition == "大雨") {
         url = "/images/10-dayu.svg";
-      }
-      else if (condition == "暴雨") {
+      } else if (condition == "暴雨") {
         url = "/images/11-baoyu.svg";
-      }
-      else if (condition == "大暴雨") {
+      } else if (condition == "大暴雨") {
         url = "/images/12-dabaoyu.svg";
-      }
-      else if (condition == "特大暴雨") {
+      } else if (condition == "特大暴雨") {
         url = "/images/13-tedabaoyu.svg";
-      }
-      else if (condition == "阵雪") {
+      } else if (condition == "阵雪") {
         url = "/images/38-zhenxuenight.svg";
-      }
-      else if (condition == "小雪") {
+      } else if (condition == "小雪") {
         url = "/images/15-xiaoxue.svg";
-      }
-      else if (condition == "中雪") {
+      } else if (condition == "中雪") {
         url = "/images/16-zhongxue.svg";
-      }
-      else if (condition == "大雪") {
+      } else if (condition == "大雪") {
         url = "/images/17-daxue.svg";
-      }
-      else if (condition == "暴雪") {
+      } else if (condition == "暴雪") {
         url = "/images/18-baoxue.svg";
-      }
-      else if (condition == "雾") {
+      } else if (condition == "雾") {
         url = "/images/19-wu.svg";
-      }
-      else if (condition == "冻雨") {
+      } else if (condition == "冻雨") {
         url = "/images/20-dongyu.svg";
-      }
-      else if (condition == "沙尘暴") {
+      } else if (condition == "沙尘暴") {
         url = "/images/21-shachenbao.svg";
-      }
-      else if (condition == "浮尘") {
+      } else if (condition == "浮尘") {
         url = "/images/30-fuchen.svg";
-      }
-      else if (condition == "扬沙") {
+      } else if (condition == "扬沙") {
         url = "/images/31-yangsha.svg";
-      }
-      else if (condition == "强沙尘暴") {
+      } else if (condition == "强沙尘暴") {
         url = "/images/32-qiangshachenbao.svg";
-      }
-      else if (condition == "霾") {
+      } else if (condition == "霾") {
         url = "/images/33-mai.svg";
-      }
-      else {
+      } else {
         url = "/images/unknow.svg";
       }
     }
